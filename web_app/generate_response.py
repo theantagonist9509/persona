2fa88804f46983f9be3e91db92252e8695a7fc82
SuvrayanBandyopadhyay <suvrayanb@gmail.com>
2025-03-06 18:53:11 +0530

    Added counselor support

--- web_app/generate_response.py
+++ web_app/generate_response.py
@@ -11,7 +11,9 @@ def genereate_response(prompt,history):
     messages = [
         (
             "system",
-            "You are a therapeutic bot who wants to know more about the patient's mental state"
+            "You are a therapeutic bot who wants to know more about the patient's mental state. "
+            "If you believe that the student needs help tell him/her to contact the counselor (9855522123)"
+            "Be as freindly as possible"
         ),  
       
         
