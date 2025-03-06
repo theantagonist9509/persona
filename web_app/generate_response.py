835152565bc358f10ff896b2146a0166fbb9a52e
SuvrayanBandyopadhyay <suvrayanb@gmail.com>
2025-03-06 16:47:20 +0530

    Added Ollama Langchain for the conversation bot

--- web_app/generate_response.py
+++ web_app/generate_response.py
@@ -1,18 +1,19 @@
-from transformers import AutoModelForCausalLM,AutoTokenizer
-import torch
+from langchain_ollama import ChatOllama
 
-model_name = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name,torch_dtype=torch.float32,device_map="auto")
+llm = ChatOllama(
+    model = "llama3.1",
+    temperature = 0
+)
+
+
 
 def genereate_response(prompt):
-    #Adding some safeguards
-    prompt += ". Respond in english"
-
-    inputs = tokenizer(prompt,return_tensors="pt").to("cpu")
-    with torch.no_grad():
-        output = model.generate(**inputs,max_new_tokens= 64,temperature=0.1)
-    
-
-    response = tokenizer.decode(output[0],skip_special_tokens=True)
-    return response    
+    messages = [
+        (
+            "system",
+            "You are a therapeutic bot who wants to know more about the patient's mental state"
+        ),  
+        ("human",prompt)
+    ]
+    msg = llm.invoke(messages)
+    return msg.content
